2b9a6771d8f0ccbc2a7e53b052715382ebf29104
jeet-oza <jeet.oza2020+git@gmail.com>
2026-06-09 15:50:46 -0400

    chore: initial project setup with Xcode template and .gitignore
    - Add .gitignore for Xcode, macOS, SPM, CocoaPods, Firebase
    - Include default SwiftUI app template (ContentView, App entry point)
    - Include XCTest and UITest targets
    - Exclude xcuserdata and DS_Store from tracking

---
+++ MentalMathApp/MentalMathApp/MentalMathAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalMathAppApp.swift
+//  MentalMathApp
+//
+//  Created by Jeet Oza on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MentalMathAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
